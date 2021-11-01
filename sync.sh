e760712df0d709fb6ee2306ba95a92c97855fcef
Andrei Kvapil <kvapss@gmail.com>
2021-11-01 16:04:39 +0100

    fix non-persistent disks transfering

--- sync.sh
+++ sync.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 for i in vmm datastore tm; do
-  (set -x; kubectl cp $i opennebula-oned-0:/var/lib/one/remotes/ -c oned)
+  (set -x; kubectl cp $i opennebula-oned-1:/var/lib/one/remotes/ -c oned)
 done
